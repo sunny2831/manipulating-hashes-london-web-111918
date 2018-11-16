e88267576e16fe025b2a7ad03cb829cae632e64d
Sunny Patel <sunny2831@hotmail.co.uk>
2018-11-16 14:26:09 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,8 +1,9 @@
+require 'pry'
 def first_challenge
   contacts = {
     "Jon Snow" => {
       name: "Jon",
-      email: "jon_snow@thewall.we", 
+      email: "jon_snow@thewall.we",
       favorite_icecream_flavors: ["chocolate", "vanilla", "mint chip"],
       knows: nil
     },
@@ -14,9 +15,16 @@ def first_challenge
   }
 
   #your code here
+  contacts.each do |person, data|
 
+    data.each do |attribute, value|
 
-  #remember to return your newly altered contacts hash!
+      if attribute == :favorite_icecream_flavors
+        value.delete_if do |flavor|
+        flavor == "strawberry"
+        end
+      end
+    end
+  end
   contacts
 end
-
